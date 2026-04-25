92c001026d13e223cc433423543ab16f44a91730
Mehmet <mehmetalikoker@outlook.com>
2026-04-25 18:14:58 +0300

    delete,drop,alter validation and chat memory  added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sqlite3
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import create_sql_agent
 from langchain_openai import ChatOpenAI
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,9 +24,21 @@ llm = ChatOpenAI(model="gpt-4o", temperature=0)
 custom_suffix = """
 Her zaman sorgu yazmadan önce veritabanı şemasını dikkatlice incele. 
 Eğer soruyla ilgili yeterli bilgi yoksa, varsayımda bulunma, kullanıcıya sor.
+Veritabanı üzerinde DROP, DELETE veya ALTER komutlarını kesinlikle kullanamazsın.
 Sonuçları her zaman kullanıcı dostu bir Türkçe ile açıkla.
 """
 
+config = {
+    "configurable": {
+        "session_id": "agenticsql_sessionID"
+    }
+}
+store = {}
+
+def get_session_history(agenticsql_sessionID: str):
+    if agenticsql_sessionID not in store:
+        store[agenticsql_sessionID] = ChatMessageHistory()
+    return store[agenticsql_sessionID]
 
 
 # create agent
@@ -35,6 +49,12 @@ agent_executor = create_sql_agent(
     verbose=True,
     custom_suffix=custom_suffix
 )
+agent_with_chat_history = RunnableWithMessageHistory(
+    agent_executor,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
 
-response = agent_executor.invoke({"input": "Veritabanındaki tabloları listele ve her tabloda kaç satır olduğunu söyle."})
+response = agent_with_chat_history.invoke({"input": "Veritabanındaki tabloları listele ve her tabloda kaç satır olduğunu söyle."},config)
 print(response["output"])
